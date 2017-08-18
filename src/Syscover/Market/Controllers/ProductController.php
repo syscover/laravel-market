450a5b7899760bee2582c6fe04782152b25eed82
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-08-18 18:30:17 +0200

    add ProductService

--- src/Syscover/Market/Controllers/ProductController.php
+++ src/Syscover/Market/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Syscover\Core\Controllers\CoreController;
 use Syscover\Market\Models\Product;
 use Syscover\Market\Models\ProductLang;
 use Syscover\Admin\Services\AttachmentService;
+use Syscover\Market\Services\ProductService;
 
 /**
  * Class ProductController
@@ -25,76 +26,8 @@ class ProductController extends CoreController
      */
     public function store(Request $request)
     {
-        if(! $request->has('id'))
-        {
-            // create new product
-            $object = Product::create([
-                'code'                  => $request->input('code'),
-                'field_group_id'        => $request->input('field_group_id'),
-                'type_id'               => $request->input('type_id'),
-                'parent_id'     => $request->input('parent_id'),
-                'weight'                => $request->input('weight'),
-                'active'                => $request->input('active'),
-                'sort'                  => $request->input('sort'),
-                'price_type_id'         => $request->input('price_type_id'),
-                'subtotal'              => $request->input('subtotal'),
-                'product_class_tax_id'  => $request->input('product_class_tax_id')
-            ]);
-
-            $id     = $object->id;
-        }
-        else
-        {
-            // create product with other language
-            $id     = $request->input('id');
-        }
-
-        // get custom fields
-        $data = [];
-        if($request->has('field_group_id'))
-        {
-            $fields = Field::where('field_group_id', $request->input('field_group_id'))->get();
-            foreach ($fields as $field)
-            {
-                $data['properties'][$field->name] = $request->input($field->name);
-            }
-        }
-
-        // create product lang
-        ProductLang::create([
-            'id'            => $id,
-            'lang_id'       => $request->input('lang_id'),
-            'name'          => $request->input('name'),
-            'slug'          => $request->input('slug'),
-            'description'   => $request->input('description'),
-            'data'          => $data
-        ]);
-
-        // update data_lang after create ProductLang because yu
-        Product::where('market_product.id', $id)->update([
-            'data_lang' => json_encode(Product::addLangDataRecord($request->input('lang_id'), $id))
-        ]);
-
-        $object = Product::builder()
-            ->where('market_product.id', $id)
-            ->where('market_product_lang.lang_id', $request->input('lang_id'))
-            ->first();
-
-        // set categories
-        $this->setCategories($object, $request);
-
-        // set attachments
-        if(is_array($request->input('attachments')))
-        {
-            // first save libraries to get id
-            $attachments = AttachmentService::storeAttachmentsLibrary($request->input('attachments'));
-
-            // then save attachments
-            AttachmentService::storeAttachments($attachments, 'storage/app/public/market/products', 'storage/market/products', $this->model, $object->id,  $object->lang_id);
-        }
-
         $response['status'] = "success";
-        $response['data']   = $object;
+        $response['data']   = ProductService::create($request->all());
 
         return response()->json($response);
     }
@@ -109,78 +42,8 @@ class ProductController extends CoreController
      */
     public function update(Request $request, $id, $lang)
     {
-        // update product
-        Product::where('market_product.id', $id)->update([
-            'code'                  => $request->input('code'),
-            'field_group_id'        => $request->input('field_group_id'),
-            'type_id'               => $request->input('type_id'),
-            'parent_id'             => $request->input('parent_id'),
-            'weight'                => $request->input('weight'),
-            'active'                => $request->input('active'),
-            'sort'                  => $request->input('sort'),
-            'price_type_id'         => $request->input('price_type_id'),
-            'subtotal'              => $request->input('subtotal'),
-            'product_class_tax_id'  => $request->input('product_class_tax_id')
-        ]);
-
-        // get custom fields
-        $data = [];
-        if($request->has('field_group_id'))
-        {
-            $fields = Field::where('field_group_id', $request->input('field_group_id'))->get();
-            foreach ($fields as $field)
-            {
-                $data['properties'][$field->name] = $request->input($field->name);
-            }
-        }
-
-        // update product lang
-        ProductLang::where('market_product_lang.id', $id)
-            ->where('market_product_lang.lang_id', $lang)
-            ->update([
-                'name'          => $request->input('name'),
-                'slug'          => $request->input('slug'),
-                'description'   => $request->input('description'),
-                'data'          => json_encode($data)
-            ]);
-
-        $object = Product::builder()
-            ->where('market_product.id', $id)
-            ->where('market_product_lang.lang_id', $lang)
-            ->first();
-
-        // categories
-        $this->setCategories($object, $request);
-
-        // set attachments
-        if(is_array($request->input('attachments')))
-        {
-            // first save libraries to get id
-            $attachments = AttachmentService::storeAttachmentsLibrary($request->input('attachments'));
-
-            // then save attachments
-            AttachmentService::updateAttachments($attachments, 'storage/app/public/market/products', 'storage/market/products', $this->model, $object->id,  $object->lang_id);
-        }
-
-        elseif(isset($object->data['properties']))
-        {
-            $data = $object->data;
-            unset($data['properties']);
-
-            // delete properties from all languages
-            ProductLang::where('product_lang.id', $id)
-                ->update([
-                    'data' => json_encode($data)
-                ]);
-
-            $object = Product::builder()
-                ->where('market_product.id', $id)
-                ->where('market_product_lang.lang_id', $lang)
-                ->first();
-        }
-
         $response['status'] = "success";
-        $response['data']   = $object;
+        $response['data']   = ProductService::update($request->all(), $id, $lang);
 
         return response()->json($response);
     }
--- src/Syscover/Market/GraphQL/Queries/ProductTaxesQuery.php
+++ src/Syscover/Market/GraphQL/Queries/ProductTaxesQuery.php
@@ -3,7 +3,6 @@
 use GraphQL\Type\Definition\Type;
 use Folklore\GraphQL\Support\Query;
 use Syscover\Core\GraphQL\ScalarTypes\ObjectType;
-use Syscover\Core\Services\SQLService;
 use Syscover\Market\Models\TaxRule;
 use Syscover\Market\Services\TaxRuleService;
 
