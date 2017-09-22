c228c3128ad2a8a6488ff06745463913e2c0109c
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2017-09-22 16:47:43 +0200

    translations

--- src/lang/en/pulsar.php
+++ src/lang/en/pulsar.php
@@ -1,13 +1,17 @@
 <?php
 
 return [
-    'message_customer_go_to_paypal'         => 'Customer go to PayPal',
-    'message_customer_go_to_tpv_error'      => 'Error to go to TPV, Error: :error',
-    'message_paypal_payment_error'          => 'PayPal payment error',
-    'message_paypal_payment_successful'     => 'PayPal payment successful',
-    'message_redsys_payment_error'          => 'TPV payment error',
-    'message_redsys_payment_successful'     => 'TPV payment successful',
-    'order_payment'                         => 'Order payment :order',
-    'paypal_item_list_description'          => 'Products',
-    'paypal_shipping_description'           => 'Shipping',
+    'downloadable'                                  => 'Downloadable',
+    'message_customer_go_to_paypal'                 => 'Customer go to PayPal',
+    'message_customer_go_to_tpv_error'              => 'Error to go to TPV, Error: :error',
+    'message_paypal_payment_error'                  => 'PayPal payment error',
+    'message_paypal_payment_successful'             => 'PayPal payment successful',
+    'message_redsys_payment_error'                  => 'TPV payment error',
+    'message_redsys_payment_successful'             => 'TPV payment successful',
+    'order_payment'                                 => 'Order payment :order',
+    'paypal_item_list_description'                  => 'Products',
+    'paypal_shipping_description'                   => 'Shipping',
+    'service'                                       => 'Service',
+    'transportable'                                 => 'Transportable',
+    'transportable_downloadable'                    => 'Transportable and downloadable',
 ];
