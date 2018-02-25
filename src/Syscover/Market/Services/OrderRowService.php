9fac1fba49da20bc0f2fd00e3fa697b00a1d7bcb
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-02-25 16:32:51 +0100

    add builder to OrderRow

--- src/Syscover/Market/Services/OrderRowService.php
+++ src/Syscover/Market/Services/OrderRowService.php
@@ -1,131 +1,82 @@
 <?php namespace Syscover\Market\Services;
 
-use Carbon\Carbon;
 use Syscover\Market\Models\OrderRow;
 
 class OrderRowService
 {
     /**
-     * @param $item
-     * @return $this|\Illuminate\Database\Eloquent\Model
+     * @param array $object
+     * @return \Syscover\Market\Models\OrderRow
      */
-    public static function create($item)
+    public static function create(array $object)
     {
-        return OrderRow::create($item);
+        return OrderRow::create(OrderRowService::builder($object));
     }
 
     /**
-     * @param $items
-     * @return bool
+     * @param   array $objects
+     * @return  bool
      */
-    public static function insert($items)
+    public static function insert($objects)
     {
-        foreach ($items as &$item)
+        $rows = [];
+        foreach ($objects as $object)
         {
-            // prevent not null values
-            if(array_key_exists('has_gift', $item) && $item['has_gift'] === null) unset($item['has_gift']);
+            if(! empty($object['data'])) $object['data'] = json_encode($object['data']);
+            if(! empty($object['tax_rules'])) $object['tax_rules'] = json_encode($object['tax_rules']);
 
-            if($item['data'] !== null) $item['data'] = json_encode($item['data']);
-            $item['tax_rules'] = json_encode($item['tax_rules']);
+            $rows[] = OrderRowService::builder($object);
         }
 
-        return OrderRow::insert($items);
+        return OrderRow::insert($rows);
     }
 
-    /**
-     * @param   array     $object     contain properties of section
-     * @param   int       $id         id of category
-     * @param   string    $lang       lang of category
-     * @return  \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null|static|static[]
-     */
-    public static function update($id, $lang, $object)
-    {
-
-    }
-
-    private static function builder(int $orderId, string $langId, $object)
+    private static function builder($object)
     {
         $object = collect($object);
         $data = [];
 
-        if($orderId)                                 $data['order_id'] = $orderId;
-        if($langId)                                  $data['lang_id'] = $langId;
-        if($object->has('product_id'))          $data['product_id'] = $object->get('product_id');
-        if($object->has('name'))                $data['name'] = $object->get('name');
-        // if($object->has('description'))                  $data['description'] = $object->get('description');
-        // if($object->has('data'))                         $data['data'] = $object->get('data');
-        if($object->has('price'))                       $data['price'] = $object->get('price');
-        if($object->has('quantity'))                    $data['quantity'] = $object->get('quantity');
-        if($object->has('subtotal'))                    $data['subtotal'] = $object->get('subtotal');
-        if($object->has('total_without_discounts'))     $data['total_without_discounts'] = $object->get('total_without_discounts');
+        if($object->has('order_id'))                                $data['order_id'] = $object->get('order_id');
+        if($object->has('lang_id'))                                 $data['lang_id'] = $object->get('lang_id');
+        if($object->has('product_id'))                              $data['product_id'] = $object->get('product_id');
 
-        if($object->has('total'))              $data['total'] = $object->get('total');
-    }
+        // product
+        if($object->has('name'))                                    $data['name'] = $object->get('name');
+        if($object->has('description'))                             $data['description'] = $object->get('description');
+        if($object->has('data'))                                    $data['data'] = $object->get('data');
 
-    public static function orderRowBuilder($lang, $orderId, $items)
-    {
-        $orderRows = [];
-        foreach ($items as $item)
-        {
-            $orderRows[] = [
-                'lang_id'                                   => $lang,
-                'order_id'                                  => $orderId,
-                'product_id'                                => $item->id,
+        // amounts
+        if($object->has('price'))                                   $data['price'] = $object->get('price');
+        if($object->has('quantity'))                                $data['quantity'] = $object->get('quantity');
+        if($object->has('subtotal'))                                $data['subtotal'] = $object->get('subtotal');
+        if($object->has('total_without_discounts'))                 $data['total_without_discounts'] = $object->get('total_without_discounts');
 
-                //****************
-                //* Product
-                //****************
-                'name'                                      => $item->name,
-                'description'                               => $item->options->product->description,
-                'data'                                      => ['product' => $item->options->product],
+        // discounts
+        if($object->has('discount_subtotal_percentage'))            $data['discount_subtotal_percentage'] = $object->get('discount_subtotal_percentage');
+        if($object->has('discount_total_percentage'))               $data['discount_total_percentage'] = $object->get('discount_total_percentage');
+        if($object->has('discount_subtotal_percentage_amount'))     $data['discount_subtotal_percentage_amount'] = $object->get('discount_subtotal_percentage_amount');
+        if($object->has('discount_total_percentage_amount'))        $data['discount_total_percentage_amount'] = $object->get('discount_total_percentage_amount');
+        if($object->has('discount_subtotal_fixed_amount'))          $data['discount_subtotal_fixed_amount'] = $object->get('discount_subtotal_fixed_amount');
+        if($object->has('discount_total_fixed_amount'))             $data['discount_total_fixed_amount'] = $object->get('discount_total_fixed_amount');
+        if($object->has('discount_amount'))                         $data['discount_amount'] = $object->get('discount_amount');
 
-                //****************
-                //* amounts
-                //****************
-                'price'                                     => $item->price,                                            // unit price without tax
-                'quantity'                                  => $item->quantity,                                         // number of units
-                'subtotal'                                  => $item->subtotal,                                         // subtotal without tax
-                'total_without_discounts'                   => $item->totalWithoutDiscounts,                            // total from row without discounts
+        // subtotal with discounts
+        if($object->has('subtotal_with_discounts'))                 $data['subtotal_with_discounts'] = $object->get('subtotal_with_discounts');
 
-                //****************
-                //* discounts
-                //****************
-                'discount_subtotal_percentage'              => $item->discountSubtotalPercentage,
-                'discount_total_percentage'                 => $item->discountTotalPercentage,
-                'discount_subtotal_percentage_amount'       => $item->discountSubtotalPercentageAmount,
-                'discount_total_percentage_amount'          => $item->discountTotalPercentageAmount,
-                'discount_subtotal_fixed_amount'            => $item->discountSubtotalFixedAmount,
-                'discount_total_fixed_amount'               => $item->discountTotalFixedAmount,
-                'discount_amount'                           => $item->discountAmount,
+        // taxes
+        if($object->has('tax_rules'))                               $data['tax_rules'] = $object->get('tax_rules');
+        if($object->has('tax_amount'))                              $data['tax_amount'] = $object->get('tax_amount');
 
-                //***************************
-                //* subtotal with discounts
-                //***************************
-                'subtotal_with_discounts'                   => $item->subtotalWithDiscounts,                            // subtotal without tax and with discounts
+        // total
+        if($object->has('total'))                                   $data['total'] = $object->get('total');
 
-                //****************
-                //* taxes
-                //****************
-                'tax_rules'                                 => $item->taxRules->values(),
-                'tax_amount'                                => $item->taxAmount,
+        // gift
+        if($object->has('has_gift'))                                $data['has_gift'] = $object->get('has_gift');
+        if($object->has('gift_from'))                               $data['gift_from'] = $object->get('gift_from');
+        if($object->has('gift_to'))                                 $data['gift_to'] = $object->get('gift_to');
+        if($object->has('gift_message'))                            $data['gift_message'] = $object->get('gift_message');
+        if($object->has('gift_comments'))                           $data['gift_comments'] = $object->get('gift_comments');
 
-                //****************
-                //* total
-                //****************
-                'total'                                     => $item->total,                                            // total with tax and discounts
-
-                //****************
-                //* gift
-                //****************
-                // to set gift, create array in options Shopping Cart, with gift key, and keys: from, to, message
-                'has_gift'                              => $item->options->gift != null? true : false,
-                'gift_from'                             => isset($item->options->gift['from'])? $item->options->gift['from'] : null,
-                'gift_to'                               => isset($item->options->gift['to'])? $item->options->gift['to'] : null,
-                'gift_message'                          => isset($item->options->gift['message'])? $item->options->gift['message'] : null,
-                'gift_comments'                         => isset($item->options->gift['comments'])? $item->options->gift['comments'] : null
-            ];
-        }
-
-        return $orderRows;
+        return $object;
     }
 }
