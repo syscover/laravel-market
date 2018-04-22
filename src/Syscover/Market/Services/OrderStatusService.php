74de3714d4f08d9e3c638d5b3a0caddb858041cf
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-22 13:18:57 +0200

    improve order statuses

--- src/Syscover/Market/Services/OrderStatusService.php
+++ src/Syscover/Market/Services/OrderStatusService.php
@@ -4,42 +4,46 @@ use Syscover\Market\Models\OrderStatus;
 
 class OrderStatusService
 {
-    /**
-     * Function to create a order status
-     * @param   array  $object
-     * @return  \Syscover\Market\Models\OrderStatus
-     * @throws  \Exception
-     */
     public static function create($object)
     {
-        if(empty($object['id']))
-        {
-            $id = OrderStatus::max('id');
-            $id++;
-            $object['id'] = $id;
-        }
+        OrderStatusService::check($object);
+
+        if(empty($object['id'])) $object['id'] = next_id(OrderStatus::class);
 
         $object['data_lang'] = OrderStatus::addDataLang($object['lang_id'], $object['id']);
 
-        return OrderStatus::create($object);
+        return OrderStatus::create(OrderStatusService::builder($object));
     }
 
-    /**
-     * @param   array     $object     contain properties of section
-     * @return  \Syscover\Market\Models\OrderStatus
-     */
     public static function update($object)
     {
-        // pass object to collection
+        OrderStatusService::check($object);
+        OrderStatus::where('ix', $object['ix'])->update(OrderStatusService::builder($object));
+        OrderStatus::where('id', $object['id'])->update(OrderStatusService::builder($object, ['active']));
+
+        return OrderStatus::find($object['ix']);
+    }
+
+    private static function builder($object, $filterKeys = null)
+    {
         $object = collect($object);
+        if($filterKeys) $object = $object->only($filterKeys);
 
-        OrderStatus::where('id', $object->get('id'))
-            ->update([
-                'name'                  => $object->get('name'),
-                'active'                => $object->get('active')
-            ]);
+        $data = [];
 
-        return OrderStatus::where('id', $object->get('id'))
-            ->first();
+        if($object->has('id'))          $data['id'] = $object->get('id');
+        if($object->has('lang_id'))     $data['lang_id'] = $object->get('lang_id');
+        if($object->has('name'))        $data['name'] = $object->get('name');
+        if($object->has('active'))      $data['active'] = $object->get('active');
+        if($object->has('data_lang'))   $data['data_lang'] = $object->get('data_lang');
+
+        return $data;
+    }
+
+    private static function check($object)
+    {
+        if(empty($object['lang_id']))   throw new \Exception('You have to define a lang_id field to create a order status');
+        if(empty($object['name']))      throw new \Exception('You have to define a name field to create a order status');
+        if(! isset($object['active']))  throw new \Exception('You have to define a active field to create a order status');
     }
 }
