8cdb8fdc26ed3ec869115795b88632d0f67870dc
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-25 23:44:20 +0200

    improve category service

--- src/Syscover/Market/Services/CategoryService.php
+++ src/Syscover/Market/Services/CategoryService.php
@@ -4,45 +4,49 @@ use Syscover\Market\Models\Category;
 
 class CategoryService
 {
-    /**
-     * Function to create a category
-     * @param   array                           $object
-     * @return  \Syscover\Market\Models\Category
-     * @throws  \Exception
-     */
     public static function create($object)
     {
-        if(empty($object['id']))
-        {
-            $id = Category::max('id');
-            $id++;
-            $object['id'] = $id;
-        }
+        if(empty($object['id'])) $object['id'] = next_id(Category::class);
 
         $object['data_lang'] = Category::addDataLang($object['lang_id'], $object['id']);
 
         return Category::create($object);
     }
 
-    /**
-     * @param   array     $object     contain properties of section
-     * @return  \Syscover\Market\Models\Category
-     */
     public static function update($object)
     {
-        // pass object to collection
+        CategoryService::check($object);
+
+        Category::where('id', $object['id'])->update(CategoryService::builder($object, ['parent_id', 'active']));
+        Category::where('ix', $object['ix'])->update(CategoryService::builder($object, ['name', 'slug', 'description']));
+
+        return Category::find($object['ix']);
+    }
+
+    private static function builder($object, $filterKeys = null)
+    {
         $object = collect($object);
+        if($filterKeys) $object = $object->only($filterKeys);
 
-        Category::where('ix', $object->get('ix'))
-            ->update([
-                'parent_id'             => $object->get('parent_id'),
-                'name'                  => $object->get('name'),
-                'slug'                  => $object->get('slug'),
-                'active'                => $object->get('active'),
-                'description'           => $object->get('description'),
-            ]);
+        $data = [];
 
-        return Category::where('id', $object->get('ix'))
-            ->first();
+        if($object->has('id'))          $data['id'] = $object->get('id');
+        if($object->has('lang_id'))     $data['lang_id'] = $object->get('lang_id');
+        if($object->has('parent_id'))   $data['parent_id'] = $object->get('parent_id');
+        if($object->has('name'))        $data['name'] = $object->get('name');
+        if($object->has('slug'))        $data['slug'] = $object->get('slug');
+        if($object->has('active'))      $data['active'] = $object->get('active');
+        if($object->has('description')) $data['description'] = $object->get('description');
+        if($object->has('data_lang'))   $data['data_lang'] = $object->get('data_lang');
+
+        return $data;
+    }
+
+    private static function check($object)
+    {
+        if(empty($object['lang_id']))   throw new \Exception('You have to define a lang_id field to create a category');
+        if(empty($object['name']))      throw new \Exception('You have to define a name field to create a category');
+        if(empty($object['slug']))      throw new \Exception('You have to define a slug field to create a category');
+        if(! isset($object['active']))  throw new \Exception('You have to define a active field to create a category');
     }
 }
