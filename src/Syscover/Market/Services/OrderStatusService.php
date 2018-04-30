d62ce365965bdce4abf105e42a6248b98064241d
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-04-30 20:00:28 +0200

    improve services

--- src/Syscover/Market/Services/OrderStatusService.php
+++ src/Syscover/Market/Services/OrderStatusService.php
@@ -6,7 +6,7 @@ class OrderStatusService
 {
     public static function create($object)
     {
-        OrderStatusService::check($object);
+        OrderStatusService::checkCreate($object);
 
         if(empty($object['id'])) $object['id'] = next_id(OrderStatus::class);
 
@@ -17,7 +17,7 @@ class OrderStatusService
 
     public static function update($object)
     {
-        OrderStatusService::check($object);
+        OrderStatusService::checkUpdate($object);
         OrderStatus::where('ix', $object['ix'])->update(OrderStatusService::builder($object));
         OrderStatus::where('id', $object['id'])->update(OrderStatusService::builder($object, ['active']));
 
@@ -29,21 +29,19 @@ class OrderStatusService
         $object = collect($object);
         if($filterKeys) $object = $object->only($filterKeys);
 
-        $data = [];
-
-        if($object->has('id'))          $data['id'] = $object->get('id');
-        if($object->has('lang_id'))     $data['lang_id'] = $object->get('lang_id');
-        if($object->has('name'))        $data['name'] = $object->get('name');
-        if($object->has('active'))      $data['active'] = $object->get('active');
-        if($object->has('data_lang'))   $data['data_lang'] = $object->get('data_lang');
-
-        return $data;
+        return $object->only('id', 'lang_id', 'name', 'active', 'data_lang')->toArray();
     }
 
-    private static function check($object)
+    private static function checkCreate($object)
     {
         if(empty($object['lang_id']))   throw new \Exception('You have to define a lang_id field to create a order status');
         if(empty($object['name']))      throw new \Exception('You have to define a name field to create a order status');
         if(! isset($object['active']))  throw new \Exception('You have to define a active field to create a order status');
+    }
+
+    private static function checkUpdate($object)
+    {
+        if(empty($object['ix'])) throw new \Exception('You have to define a ix field to update a order status');
+        if(empty($object['id'])) throw new \Exception('You have to define a id field to update a order status');
     }
 }
