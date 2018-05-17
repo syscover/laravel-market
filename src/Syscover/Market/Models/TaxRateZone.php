61c341e1a6ce03c33f9eb3edcc707ca2628ec9e8
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-05-17 11:39:50 +0200

    add cast to float in tax rate zone

--- src/Syscover/Market/Models/TaxRateZone.php
+++ src/Syscover/Market/Models/TaxRateZone.php
@@ -14,7 +14,9 @@ class TaxRateZone extends CoreModel
 	protected $table        = 'market_tax_rate_zone';
     protected $fillable     = ['id', 'name', 'country_id', 'territorial_area_1_id', 'territorial_area_2_id', 'territorial_area_3_id', 'zip', 'tax_rate'];
     public $with            = ['country.lang'];
-
+    protected $casts        = [
+        'tax_rate' => 'float'
+    ];
     private static $rules   = [];
 
     public static function validate($data)
