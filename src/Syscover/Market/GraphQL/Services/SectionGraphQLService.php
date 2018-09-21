9d9d5a5e97776980ddfdbadea6debdf80d8a30ea
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-21 18:54:09 +0200

    add sections

---
+++ src/Syscover/Market/GraphQL/Services/SectionGraphQLService.php
@@ -0,0 +1,11 @@
+<?php namespace Syscover\Market\GraphQL\Services;
+
+use Syscover\Core\GraphQL\Services\CoreGraphQLService;
+use Syscover\Market\Models\Section;
+use Syscover\Market\Services\SectionService;
+
+class SectionGraphQLService extends CoreGraphQLService
+{
+    protected $model = Section::class;
+    protected $service = SectionService::class;
+}
