98be094cafb7159c9a0eb82dc8de3481b866d827
Jose Carlos R. Palacín (Syscover) <cpalacin@syscover.com>
2018-09-27 19:45:17 +0200

    Delete scopePaginationBuilder No es necesario realizar group by  para el paginado, crea filas duplicadas debido al SET sql_mode = 'only_full_group_by'

--- src/Syscover/Market/Models/TaxRule.php
+++ src/Syscover/Market/Models/TaxRule.php
@@ -49,34 +49,32 @@ class TaxRule extends CoreModel
 
     public function scopePaginationBuilder($query)
     {
-        return $query
-            ->join('market_tax_rules_customer_class_taxes', 'market_tax_rule.id', '=', 'market_tax_rules_customer_class_taxes.tax_rule_id')
-            ->join('market_tax_rules_product_class_taxes', 'market_tax_rule.id', '=', 'market_tax_rules_product_class_taxes.tax_rule_id')
-            ->join('market_customer_class_tax', 'market_tax_rules_customer_class_taxes.customer_class_tax_id', '=', 'market_customer_class_tax.id')
-            ->join('market_product_class_tax', 'market_tax_rules_product_class_taxes.product_class_tax_id', '=', 'market_product_class_tax.id')
-            ->addSelect(
-                'market_product_class_tax.*',
-                'market_customer_class_tax.*',
-                'market_tax_rules_product_class_taxes.*',
-                'market_tax_rules_customer_class_taxes.*',
-                'market_tax_rule.*',
-                'market_product_class_tax.id as market_product_class_tax_id',
-                'market_customer_class_tax.id as market_customer_class_tax_id'
-            )
-            ->groupBy(
-                'market_tax_rule.id',
-                'market_tax_rule.name',
-                'market_tax_rule.translation',
-                'market_tax_rule.priority',
-                'market_tax_rule.sort',
-                'market_product_class_tax.id',
-                'market_customer_class_tax.id'
-            );
-    }
+        return $query;
+        // comment code
+        // not necessary for paginate, duplicate rows for group by
 
-    public function scopeCalculateFoundRows($query)
-    {
-        return $query->select(DB::raw('SQL_CALC_FOUND_ROWS market_tax_rule.id'));
+//            ->join('market_tax_rules_customer_class_taxes', 'market_tax_rule.id', '=', 'market_tax_rules_customer_class_taxes.tax_rule_id')
+//            ->join('market_tax_rules_product_class_taxes', 'market_tax_rule.id', '=', 'market_tax_rules_product_class_taxes.tax_rule_id')
+//            ->join('market_customer_class_tax', 'market_tax_rules_customer_class_taxes.customer_class_tax_id', '=', 'market_customer_class_tax.id')
+//            ->join('market_product_class_tax', 'market_tax_rules_product_class_taxes.product_class_tax_id', '=', 'market_product_class_tax.id')
+//            ->addSelect(
+//                'market_product_class_tax.*',
+//                'market_customer_class_tax.*',
+//                'market_tax_rules_product_class_taxes.*',
+//                'market_tax_rules_customer_class_taxes.*',
+//                'market_tax_rule.*',
+//                'market_product_class_tax.id as market_product_class_tax_id',
+//                'market_customer_class_tax.id as market_customer_class_tax_id'
+//            )
+//            ->groupBy(
+//                'market_tax_rule.id',
+//                'market_tax_rule.name',
+//                'market_tax_rule.translation',
+//                'market_tax_rule.priority',
+//                'market_tax_rule.sort',
+//                'market_product_class_tax.id',
+//                'market_customer_class_tax.id'
+//            );
     }
 
     public function tax_rate_zones()
