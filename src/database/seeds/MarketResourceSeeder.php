cb24d040036f37df728cbcd01f83cea52ed24392
Carlos Rodríguez Palacín <cpalacin@digitalh2.com>
2019-03-10 15:06:41 +0100

    set resources in menu

--- src/database/seeds/MarketResourceSeeder.php
+++ src/database/seeds/MarketResourceSeeder.php
@@ -8,23 +8,39 @@ class MarketResourceSeeder extends Seeder {
     public function run()
     {
         Resource::insert([
-            ['id' => 'market',                          'name' => 'Market Package',                     'package_id' => 150],
-            ['id' => 'market-cart-price-rule',          'name' => 'Marketing -- Cart price rule',       'package_id' => 150],
-            ['id' => 'market-category',                 'name' => 'Categories',                         'package_id' => 150],
-            ['id' => 'market-order',                    'name' => 'Orders',                             'package_id' => 150],
-            ['id' => 'market-order-status',             'name' => 'Order status',                       'package_id' => 150],
-            ['id' => 'market-payment-method',           'name' => 'Payment methods',                    'package_id' => 150],
-            ['id' => 'market-product',                  'name' => 'Products',                           'package_id' => 150],
-            ['id' => 'market-tax',                      'name' => 'Taxes',                              'package_id' => 150],
-            ['id' => 'market-tax-customer',             'name' => 'Taxes -- Customer class tax',        'package_id' => 150],
-            ['id' => 'market-tax-customer-group',       'name' => 'Taxes -- Group Customer class tax',  'package_id' => 150],
-            ['id' => 'market-tax-product',              'name' => 'Taxes -- Product class tax',         'package_id' => 150],
-            ['id' => 'market-tax-rate-zone',            'name' => 'Taxes -- Tax rate zone',             'package_id' => 150],
-            ['id' => 'market-tax-rule',                 'name' => 'Taxes -- Tax rule',                  'package_id' => 150],
-            ['id' => 'market-tpv',                      'name' => 'TPVs',                               'package_id' => 150],
-            ['id' => 'market-tpv-paypal',               'name' => 'TPVs -- PayPal',                     'package_id' => 150],
-            ['id' => 'market-tpv-paypal-setting',       'name' => 'TPVs -- PayPal -- Setting',          'package_id' => 150],
-            ['id' => 'market-tpv-paypal-web-profile',   'name' => 'TPVs -- PayPal -- Web profile',      'package_id' => 150],
+            ['id' => 'market',                          'name' => 'Market Package',                                 'package_id' => 150],
+
+            // Sales
+            ['id' => 'market-sale',                     'name' => 'Sales',                                          'package_id' => 150],
+            ['id' => 'market-order',                    'name' => 'Orders',                                         'package_id' => 150],
+
+            // Catalog
+            ['id' => 'market-catalog',                  'name' => 'Catalog',                                        'package_id' => 150],
+            ['id' => 'market-product',                  'name' => 'Products',                                       'package_id' => 150],
+            ['id' => 'market-category',                 'name' => 'Categories',                                     'package_id' => 150],
+            ['id' => 'market-section',                  'name' => 'Sections',                                       'package_id' => 150],
+            ['id' => 'market-warehouse',                'name' => 'Warehouse',                                      'package_id' => 150],
+
+            // Marketing
+            ['id' => 'market-marketing',                'name' => 'Marketing',                                      'package_id' => 150],
+            ['id' => 'market-cart-price-rule',          'name' => 'Marketing -- Cart price rule',                   'package_id' => 150],
+
+            // Payment Gateway
+            ['id' => 'market-tpv',                      'name' => 'Payment Gateway',                               'package_id' => 150],
+            ['id' => 'market-tpv-paypal-web-profile',   'name' => 'Payment Gateway -- PayPal -- Web profile',      'package_id' => 150],
+
+            // Taxes
+            ['id' => 'market-tax',                      'name' => 'Taxes',                                          'package_id' => 150],
+            ['id' => 'market-tax-rule',                 'name' => 'Taxes -- Tax rule',                              'package_id' => 150],
+            ['id' => 'market-tax-rate-zone',            'name' => 'Taxes -- Tax rate zone',                         'package_id' => 150],
+            ['id' => 'market-tax-product',              'name' => 'Taxes -- Product class tax',                     'package_id' => 150],
+            ['id' => 'market-tax-customer',             'name' => 'Taxes -- Customer class tax',                    'package_id' => 150],
+            ['id' => 'market-tax-customer-group',       'name' => 'Taxes -- Group Customer class tax',              'package_id' => 150],
+
+            // Preference
+            ['id' => 'market-preference',               'name' => 'Preferences',                                    'package_id' => 150],
+            ['id' => 'market-payment-method',           'name' => 'Payment methods',                                'package_id' => 150],
+            ['id' => 'market-order-status',             'name' => 'Order status',                                   'package_id' => 150],
 
         ]);
     }
